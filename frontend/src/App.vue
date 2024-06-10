89d741b01980939794689d37d46c1b415b4bcd6a
sonwon <uoun456852@gmail.com>
2024-06-10 18:25:35 +0900

    AccountRegistration init

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,9 +3,15 @@
     <router-view></router-view>
   </div>
 </template>
+
 <script>
+import Sidebar from './components/sidebar/Sidebar.vue';
+
 export default {
   name : 'App',
+  components : {
+    Sidebar
+  }
 }
 </script>
 <style lang="">
